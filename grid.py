7a9b2ad5e3a84c42f79013537411f354615f80f3
Kinyanjui Wangonya <kwangonya@gmail.com>
2018-11-22 17:01:23 +0300

    random snippets, add gitignore

---
+++ grid.py
@@ -0,0 +1,12 @@
+def draw_grid_hor():
+    return '+----+----+'
+
+def draw_grid_ver():
+    print ('|    |    |')* 3
+
+if __name__ == '__main__':
+    print(draw_grid_hor())
+    print(draw_grid_ver())
+    print(draw_grid_hor())
+    print(draw_grid_ver())
+    print(draw_grid_hor())
